82d002759948502142de0d7b38a051879f53127a
dafik <zbyszek.wieczorek@gmail.com>
2026-03-20 20:37:54 +0100

    debug: add beat_update log

--- src/hooks/useBeatTempo.tsx
+++ src/hooks/useBeatTempo.tsx
@@ -32,6 +32,7 @@ const useBeatTempo = () => {
   const stableBpmRef = useRef(0)
 
   const handleBeatUpdate = useCallback((data: BeatUpdateEvent) => {
+    console.log('[useBeatTempo] Received beat_update:', data)
     const now = Date.now()
 
     if (data.bpm > 0) {
